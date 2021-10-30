2dbf0e4dd4437a81dfe3f782a1a0638a966d4827
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-31 02:15:19 +0300

    removed keys

--- z_neighborhood/models.py
+++ z_neighborhood/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Neighborhood(models.Model):
+    """
+    Neighborhood model
+    """
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    population = models.IntegerField()
+    police = models.IntegerField()
+    health = models.IntegerField()
+    education = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
