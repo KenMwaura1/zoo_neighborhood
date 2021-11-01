7a7465364175d64cf1b6e8750518004046f35a92
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-01 03:29:03 +0300

    Created views for the home page and viewing all neighborhoods

--- z_neighborhood/views.py
+++ z_neighborhood/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+from .forms import RegisterForm
+from .models import NeighborHood
+
+
+@login_required(login_url='/login/')
+def home(request):
+    return render(request, 'z_neighborhood/home.html')
+
+
+def neighborhood(request):
+    all_neighborhoods = NeighborHood.get_all_neighborhoods()
+    return render(request, 'z_neighborhood/neighborhood.html', {'all_neighborhoods': all_neighborhoods})
+
+
+
+def profile(request):
+    return None
