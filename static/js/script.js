926f7c2a4666ed7bab9b5c61a8e67f369c3cd0e5
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-02 00:23:29 +0300

    migration and images + favicon

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$('#trigger').click(function () {
+    $('#basicExample').modal({show : true});
+});
+copyLink =(element) => {
+    document.getElementById(element).select();
+    document.execCommand("copy");
+}
+
+ScrollTo = () =>  {
+    const links = document.querySelectorAll('.scroll');
+    links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+scrollAnchors = (e, respond = null) => {
+    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+    e.preventDefault();
+    let targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+    const targetAnchor = document.querySelector(targetID);
+    if (!targetAnchor) return;
+    const originalTop = distanceToTop(targetAnchor);
+    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+    const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+            targetAnchor.tabIndex = '-1';
+            targetAnchor.focus();
+            window.history.pushState('', '', targetID);
+            clearInterval(checkIfDone);
+        }
+    }, 3000);
+}
