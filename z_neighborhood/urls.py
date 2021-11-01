c0f7aacabe13e57e09ab0ff1740bf8338810a924
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-01 23:44:12 +0300

    Updated the display of neighborhood businesses

--- z_neighborhood/urls.py
+++ z_neighborhood/urls.py
@@ -3,18 +3,20 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit_profile'),
     path('neighborhood', views.neighborhood, name='neighborhood'),
-    path('neighborhood/<int:neighborhood_id>', views.neighborhood, name='neighborhood'),
-    # path('neighborhood/<int:neighborhood_id>/new', views.new_business, name='new_business'),
-    # path('neighborhood/<int:neighborhood_id>/<int:business_id>', views.business, name='business'),
-    # path('neighborhood/<int:neighborhood_id>/<int:business_id>/edit', views.edit_business, name='edit_business'),
-    # path('neighborhood/<int:neighborhood_id>/<int:business_id>/delete', views.delete_business,
-    # name='delete_business'), path('neighborhood/<int:neighborhood_id>/<int:business_id>/new_review',
-    # views.new_review, name='new_review'), path(
-    # 'neighborhood/<int:neighborhood_id>/<int:business_id>/<int:review_id>', views.review, name='review'),
-    # path('neighborhood/<int:neighborhood_id>/<int:business_id>/<int:review_id>/edit', views.edit_review,
-    # name='edit_review'), path('neighborhood/<int:neighborhood_id>/<int:business_id>/<int:review_id>/delete',
-    # views.delete_review, name='delete_review'),
+    path('neighborhood/<int:hood_id>', views.hood_details, name='hood_details'),
+    path('search/', views.search_business, name='search'),
+    path('add-hood', views.add_hood, name='add-hood'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('neighborhood/<int:neighborhood_id>/members', views.hood_members, name='members'),
+    path('neighborhood/<int:neighborhood_id>/business', views.hood_business, name='business'),
 
     ]
